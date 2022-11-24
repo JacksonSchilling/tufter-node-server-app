5a9347c14bc9dcdaad12c43018ed33490938a494
Jackson Schilling <jackson.schilling99@gmail.com>
2022-11-24 12:51:12 -0500

    first commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import UserController
     from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb://localhost:27017/tuiter');
 const app = express();
 app.use(cors())
 app.use(express.json());
@@ -14,3 +16,6 @@ HelloController(app);
 UserController(app);
 app.listen(process.env.PORT || 4000);
 
+const CONNECTION_STRING = 'mongodb+srv://jacksonschilling:mezduz-2hocde-kybciG@cluster0.vdaromy.mongodb.net/?retryWrites=true&w=majority'
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
